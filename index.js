46c34193497d1798daca3f7f197e9922aaa49b31
Yasmine Hartung <y.hartung@gmail.com>
2019-02-04 08:13:54 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -10,7 +10,13 @@ function setCart(c) {
 }
 
 function addToCart(item) {
- // write your code here
+  var itemToCart = [{
+    itemName: `${item}`,
+    itemPrice: Math.floor(Math.random())
+
+  }];
+ cart.push(itemToCart);
+ return itemToCart;
 }
 
 function viewCart() {
