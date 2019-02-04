893c4de8e34959fe98d4b420639a26e319f51f1c
Yasmine Hartung <y.hartung@gmail.com>
2019-02-04 08:33:43 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -48,7 +48,13 @@ function total() {
 }
 
 function removeFromCart(item) {
-  // write your code here
+for (var i = 0; i < cart.length; i++) {
+  if ( item == cart[i].itemName ){
+    cart.splice( i , 1 );
+    return cart;
+  }
+}
+    return "That item is not in your cart."
 }
 
 function placeOrder(cardNumber) {
